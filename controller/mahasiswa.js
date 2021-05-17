88886bec14ed75db6402f4eae10036e24aeb6487
Hidayat <hidayat@Hidayats-MacBook-Pro.local>
2021-05-17 14:50:49 +0700

    crud rest nodejs

--- controller/mahasiswa.js
+++ controller/mahasiswa.js
@@ -32,3 +32,50 @@ exports.mahasiswaId = function (req, res) {
     }
   );
 };
+
+// menambahkan data
+exports.addMahasiswa = function (req, res) {
+  const { nim, nama, jurusan } = req.body;
+  connection.query(
+    "INSERT INTO mahasiswa (nim, nama, jurusan) VALUES (?,?,?)",
+    [nim, nama, jurusan],
+    function (err, result) {
+      if (err) {
+        console.log(err);
+      } else {
+        response.ok("Data berhasil di input", res);
+      }
+    }
+  );
+};
+
+// update data
+exports.updateMahasiswa = function (req, res) {
+  const { id, nim, nama, jurusan } = req.body;
+  connection.query(
+    "UPDATE mahasiswa SET nim=?, nama=?, jurusan=? WHERE id=?",
+    [nim, nama, jurusan, id],
+    function (err, result) {
+      if (err) {
+        console.log(err);
+      } else {
+        response.ok("Data berhasil di ubah", res);
+      }
+    }
+  );
+};
+
+// delete data
+exports.deleteMahasiswa = function (req, res) {
+  var id = req.body.id;
+  connection.query(
+    "DELETE FROM mahasiswa WHERE id=?",
+    [id],
+    function (err, result) {
+      if (err) {
+        console.log(err);
+      }
+      response.ok("Data berhasil dihapus", res);
+    }
+  );
+};
--- routes.js
+++ routes.js
@@ -5,4 +5,7 @@ module.exports = function (app) {
   app.route("/").get(controller.index);
   app.route("/mahasiswa").get(controller.tampilMahasiswa);
   app.route("/mahasiswa/:id").get(controller.mahasiswaId);
+  app.route("/tambah").post(controller.addMahasiswa);
+  app.route("/ubah").put(controller.updateMahasiswa);
+  app.route("/hapus/:id").delete(controller.deleteMahasiswa);
 };
