78e43395f3e7eb8dc0def35e0d1f1f6413d72e1a
Hidayat <hidayat@Hidayats-MacBook-Pro.local>
2021-05-19 07:55:26 +0700

    login user with jwt

--- middleware/auth.js
+++ middleware/auth.js
@@ -41,3 +41,53 @@ exports.registrasi = function (req, res) {
     }
   });
 };
+
+// controller untuk login
+exports.login = function (req, res) {
+  const post = {
+    email: req.body.email,
+    password: req.body.password,
+  };
+  var check = "SELECT * FROM ?? WHERE ??=? AND ??=?";
+  var table = ["user", "email", post.email, "password", md5(post.password)];
+  query = mysql.format(check, table);
+  connection.query(query, function (error, result) {
+    if (error) {
+      console.log(error);
+    } else {
+      if (result.length == 1) {
+        var token = jwt.sign({ result }, config.secret, {
+          expiresIn: "2400000",
+        });
+
+        id_user = result[0].id;
+
+        var data = {
+          id_user: id_user,
+          access_token: token,
+          ip_address: ip.address(),
+        };
+        var query = "INSERT INTO ?? SET ?";
+        var table = ["akses_token"];
+        query = mysql.format(query, table);
+        connection.query(query, data, function (error, result) {
+          if (error) {
+            console.log(error);
+          } else {
+            res.json({
+              success: true,
+              message: "Token JWT terintegrasi",
+              token: token,
+              user: data.id_user,
+            });
+          }
+        });
+      } else {
+        res.json({
+          error: true,
+          message: "Email dan password salah",
+        });
+      }
+    }
+  });
+};
